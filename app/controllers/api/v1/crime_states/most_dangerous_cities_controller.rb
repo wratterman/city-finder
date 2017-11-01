be35040ba7a45a991761ec97477377686839dd8c
wratterman <will.ratterman@gmail.com>
2017-10-31 21:55:13 -0600

    changed params for searching for states

--- app/controllers/api/v1/crime_states/most_dangerous_cities_controller.rb
+++ app/controllers/api/v1/crime_states/most_dangerous_cities_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::CrimeStates::MostDangerousCitiesController < ApplicationControlle
   before_action :set_crime, only: [:index]
 
   def index
-    state = State.find(params[:id])
+    state = State.find_by(name: params[:state].downcase.capitalize)
     @cities = City.most_dangerous_cities_for(@crime, state.id)
     formatted = {:id => state.id, :name => state.name, :most_dangerous_cities => @cities}
     render json: formatted
--- config/routes.rb
+++ config/routes.rb
@@ -6,9 +6,9 @@ Rails.application.routes.draw do
   get 'api/v1/crime_states/:id/cities', to: "api/v1/crime_states#show"
   get 'api/v1/crime_states/:id/cities/:city_id', to: "api/v1/crime_states/crime_cities#show", as: :city
   get 'api/v1/most_dangerous_cities', to: "api/v1/most_dangerous_cities#index"
-  get 'api/v1/crime_states/:id/most_dangerous_cities', to: "api/v1/crime_states/most_dangerous_cities#index"
+  get 'api/v1/crime_states/:state/most_dangerous_cities', to: "api/v1/crime_states/most_dangerous_cities#index"
   get 'api/v1/safest_cities', to: "api/v1/safest_cities#index"
-  get 'api/v1/crime_states/:id/safest_cities', to: "api/v1/crime_states/safest_cities#index"
+  get 'api/v1/crime_states/:state/safest_cities', to: "api/v1/crime_states/safest_cities#index"
   get 'api/v1/avg_weekly_reports/', to: "api/v1/states#index", as: :states
   get 'api/v1/avg_weekly_reports/:id', to: "api/v1/states#show", as: :state
 
