2d0a1f280079b05088090c965360ecafafd188b7
wratterman <will.ratterman@gmail.com>
2017-10-31 21:22:58 -0600

    eliminated Date

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,6 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 require 'csv'
-require 'Date'
 
 def seed_crimes
   CSV.foreach("./db/csvs/FBI-Crime-Data.csv", :headers => true) do |row|
