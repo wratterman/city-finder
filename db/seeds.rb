d006386b454c4b9e812119920b1ff8b54b7b7fb7
wratterman <will.ratterman@gmail.com>
2017-10-31 21:21:07 -0600

    fixed case sensitivity

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-require 'CSV'
+require 'csv'
 require 'Date'
 
 def seed_crimes
