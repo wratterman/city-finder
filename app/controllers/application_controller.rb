90a11e22b1b0feebc3f9c7debd9ac7278ec28a02
wratterman <will.ratterman@gmail.com>
2018-02-20 18:40:11 -0700

    added CORs filter after action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  after_action :cors_set_access_control_headers
+
+  def cors_set_access_control_headers
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    response.headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, PATCH, DELETE, OPTIONS'
+    response.headers['Access-Control-Allow-Headers'] = 'Origin, Content-Type, Accept, Authorization, Token, Auth-Token, Email'
+    response.headers['Access-Control-Max-Age'] = "1728000"
+  end
 end
