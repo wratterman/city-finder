7727dd1f014caf5c071822b8a9edf4379580f975
wratterman <will.ratterman@gmail.com>
2018-02-20 18:27:05 -0700

    got charts working

--- config/application.rb
+++ config/application.rb
@@ -27,5 +27,9 @@ module CityFinder
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+    config.action_dispatch.default_headers = {
+      'Access-Control-Allow-Origin' => '*',
+      'Access-Control-Request-Method' => %w{GET POST OPTIONS}.join(",")
+    }
   end
 end
